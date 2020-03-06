56599327655fd231d30103e9aa484ee6d7f942e0
MRSolutionsRD <61673292+MRSolutionsRD@users.noreply.github.com>
2020-03-06 13:02:52 -0400

    Add files via upload

---
+++ Pedir Nombre/Pedir Nombre/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Pedir_Nombre
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string nom;
+            Console.WriteLine("Cuál es tu nombre?");
+            nom = Console.ReadLine();
+
+
+            int ed;
+            Console.WriteLine("Qué edad tienes?");
+            ed = Convert.ToInt32 (Console.ReadLine());
+
+            Console.WriteLine("Te llamas " + nom+ " y tienes " + ed + " años de edad.");
+            
+
+            Console.ReadKey();
+
+        
+        
+        }
+    }
+}
